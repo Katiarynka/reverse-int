d96c48daff78c902cc1af35603d4d12f0ffcc6e1
Katiarynka <katia.tsygankova@gmail.com>
2021-08-16 21:35:56 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+	let string = Math.abs(n).toString()
+	let reversString = '';
+	for (i = 0; i < string.length; i++)
+	{reversString = `${string[i]}${reversString}`} 
+	return +reversString	  
 }
